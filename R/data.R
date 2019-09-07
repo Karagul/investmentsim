325483e9bcd9c65d93cfb3f1af6ab34e3e8e54e7
ryanholbrook <44940142+ryanholbrook@users.noreply.github.com>
2019-09-07 05:18:37 -0500

    Create simulated data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Simulated Financial Returns
+#'
+#' This data set contains two series of fictional returns from the years 1928
+#' to 2018. One series is meant to represent a stock and the other is meant
+#' to represent a bond.
+#'
+#' @format A data frame with 91 rows and 2 variables:
+#' \describe{
+#'   \item{Stock.Returns}{fictional yearly stock percent returns from 1928 to 2018}
+#'   \item{Bond.Returns}{fictional yearly bond percent returns from 1928 to 2918}
+#' }
+"simreturns"
